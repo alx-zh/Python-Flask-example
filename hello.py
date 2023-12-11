a73d367eedeec642f3373ed88d91c5a7f4494386
Aleksei Zhabin <alxzh77@gmail.com>
2023-12-11 13:04:47 +0300

    11-12-2023

--- hello.py
+++ hello.py
@@ -2,16 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("nwfiles/templates/index.html")
+    return 'Index Page'
+
+
+@app.route('/test')
+def test():
+    return render_template("index.html")
+
 
 @app.route("/about")
 def get_page_about():
-    return render_template("about.html", h1 = "О приложении")
-
+    return render_template("about.html", 
+                           h1 = "О приложении",
+                           content = "Page contains information text")
 
 """ 
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+
     def hello_world():
     return "<p>Hello, World!</p>"
 """
+
+if __name__ == '__main__':
+    app.run(debug=True)
